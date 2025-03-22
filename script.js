3d43a908721816aa5d7265ee7bade2b5167f6523
evmatv <evmatv92@gmail.com>
2025-03-22 11:27:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+let player = {
+    x: 50,
+    y: 300,
+    width: 50,
+    height: 50,
+    color: 'blue', // Цвет по умолчанию, если изображение не загружено
+    gravity: 0.5,
+    jumpPower: -10,
+    velocityY: 0,
+    isJumping: false,
+    jumpCount: 0,
+    image: new Image() // Изображение игрока
+};
+
+let coins = [];
+let score = 0;
+
+// Загрузка изображения игрока
+player.image.src = 'path/to/your/player-image.png'; // Замените на путь к вашему изображению
+
+function createCoin() {
+    const coin = {
+        x: Math.random() * (canvas.width - 30),
+        y: Math.random() * (canvas.height - 50),
+        width: 30,
+        height: 30,
+        collected: false,
+        image: new Image() // Изображение монеты
+    };
+    coin.image.src = 'path/to/your/coin-image.png'; // Замените на путь к вашему изображению
+    coins.push(coin);
+}
+
+function drawPlayer() {
+    ctx.drawImage(player.image, player.x, player.y, player.width, player.height);
+}
+
+function drawCoins() {
+    coins.forEach(coin => {
+        if (!coin.collected) {
+            ctx.drawImage(coin.image, coin.x, coin.y, coin.width, coin.height);
+        }
+    });
+}
+
+function update() {
+    player.velocityY += player.gravity;
+    player.y += player.velocityY;
+
+    if (player.y + player.height >= canvas.height) {
+        player.y = canvas.height - player.height;
+        player.isJumping = false;
+        player.jumpCount = 0;
+    }
+
+    coins.forEach(coin => {
+        if (!coin.collected && player.x < coin.x + coin.width && player.x + player.width > coin.x && player.y < coin.y + coin.height && player.y + player.height > coin.y) {
+            coin.collected = true;
+            score++;
+            // Эффект сбора монет
+            ctx.fillStyle = 'yellow';
+            ctx.fillRect(coin.x, coin.y, coin.width, coin.height);
+            setTimeout(() => {
+                ctx.clearRect(coin.x, coin.y, coin.width, coin.height);
+            }, 200);
+        }
+    });
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayer();
+    drawCoins();
+    ctx.fillStyle = 'black';
+    ctx.fillText('Счет: ' + score, 10, 20);
+}
+
+function moveLeft() {
+    player.x -= 5;
+}
+
+function moveRight() {
+    player.x += 5;
+}
+
+function jump() {
+    if
